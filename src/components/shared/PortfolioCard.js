dee865487678ad94339d439df68af55c26112b84
jaclynlenee <jaclynlenee@gmail.com>
2020-08-05 22:53:56 -0700

    Add Case Study Card style

--- src/components/shared/PortfolioCard.js
+++ src/components/shared/PortfolioCard.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const PortfolioCard = ({ projectName, projectTags, link, image }) => (
-	<Link to={link}>
-		<div className="Portfolio__Card">
-		  <div className="Portfolio__CardThumbnail">
+	<Link to={link} className="Portfolio__Card">
+	  <div className="Portfolio__CardThumbnail">
 				<img src={image} />
-				<h2>{projectName}</h2>
-				{ projectTags.map(tag => <p>{tag}</p>) }
-		  </div>
+	  </div>
+		<div className="Portfolio_CardContent">
+			<h6>{projectName}</h6>
+			<div className="Portfolio_CardTags">
+				{ projectTags.map(tag => <span>{tag}</span>) }
+			</div>
 		</div>
 	</Link>
 )
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -8,6 +8,7 @@ import DaohausLogo from '../images/projects/daohaus__logo--white.png'
 import PokemolLogo from '../images/projects/pokemol__brand--invert.png'
 import MetaCartelLogo from '../images/projects/meta_chill.png'
 import MolochLogo from '../images/projects/moloch__logo--red.png'
+import ImagePlaceholder from '../images/image-placeholder.png'
 
 import IconHelm from '../components/shared/icons/IconHelm'
 import IconAxes from '../components/shared/icons/IconAxes'
@@ -18,7 +19,7 @@ import PortfolioCard from '../components/shared/PortfolioCard'
 
 const PortfolioPage = () => (
 	<Layout>
-      <div className="Hero Portfolio BackgroundImage" name="portfolio">
+      <div className="Hero BackgroundImage">
         <div className="Hero__Contents">
           <h1>
             Portfolio
@@ -29,24 +30,38 @@ const PortfolioPage = () => (
         </div>
         <div className="HeroBorder" />
       </div>
-      <div className="Block Portfolio">
-        <div className="Block__Contents">
-          <div className="Portfolio__Cards">
-            <div className="Row1">
-			        <PortfolioCard
-								projectName="Cool Project"
-								projectTags={['Tag1', 'Tag2']}
-								link="/case-studies/tellor"
-								image={DaohausLogo}
-							/>
-							<PortfolioCard
-								projectName="Boring Project"
-								projectTags={['Boring']}
-								link="/case-studies/tellor"
-								image={MolochLogo}
-							/>
-          	</div>
-          </div>
+      <div className="Column">
+        <div className="Portfolio__Cards">
+	        <PortfolioCard
+						projectName="Tellor"
+						projectTags={['Design', 'Full-Stack Dev']}
+						link="/case-study/tellor"
+						image={ImagePlaceholder}
+					/>
+					<PortfolioCard
+						projectName="Minion"
+						projectTags={['UX/UI', 'Code Audit', 'Full-stack Dev']}
+						link="/case-study/tellor"
+						image={ImagePlaceholder}
+					/>
+					<PortfolioCard
+						projectName="Stake on Me"
+						projectTags={['UI/UX', 'Copywriting', 'Full-stack Dev']}
+						link="/case-study/tellor"
+						image={ImagePlaceholder}
+					/>
+					<PortfolioCard
+						projectName="Wrapeth"
+						projectTags={['UI/UX', 'Visual Design', 'Full-stack Dev']}
+						link="/case-study/tellor"
+						image={ImagePlaceholder}
+					/>
+					<PortfolioCard
+						projectName="Aragon Metrics Dashboard"
+						projectTags={['UI Design', 'Frontend Dev', 'Subgraph Dev']}
+						link="/case-study/tellor"
+						image={ImagePlaceholder}
+					/>
         </div>
       </div>
     </Layout>
