cb82975f800df09a840d707bbaef53d3f007ff50
jaclynlenee <jaclynlenee@gmail.com>
2020-10-23 17:50:23 -0700

    Fix typo

--- src/data/caseStudies/SBV.js
+++ src/data/caseStudies/SBV.js
@@ -14,7 +14,7 @@ export default {
   headerImage: headerImage,
   fullVideo: videoSrc,
   videoScreen: videoScreen,
-  challengeText: 'SBV approached Raid Guild seeking guidance on the structure of their new DAO. The challenge was to determine which DAO structure best aligned with the features they wished to include. Additionally, SBV had no prior branding or website and would need Raid Guild to handle that as well.s',
+  challengeText: 'SBV approached Raid Guild seeking guidance on the structure of their new DAO. The challenge was to determine which DAO structure best aligned with the features they wished to include. Additionally, SBV had no prior branding or website and would need Raid Guild to handle that as well.',
   approachText: 'The team consisted of two members. Burrrata, who has significant experience with both Moloch and Aragon DAOs, would consult with the team to determine the DAO structure. Darren, who has been designing within the crypto space since 2015, would work with the team to develop a quick brand and landing page. ',
   solutionTexts: ['The final solution delivered branding, design patterns and a landing page along with a report guiding the structure of the final DAO. SBV is currently working with The LAO to finalize the details of their decentralized organization.'],
   activitiesTexts: [
